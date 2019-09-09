50e77a77e24ab3288060279014507b5d63aa0ba1
peln <pern9378@stud.kea.dk>
2019-09-09 14:56:08 +0200

    register not working with form

--- register.js
+++ register.js
@@ -3,7 +3,7 @@ $('#frmRegister').submit(function(){
     console.log('signing up...')
     $.ajax({
         method: "POST",
-        url: '../voting-machine/register.php',
+        url: 'register.php',
         //key: whatever is in the form - convert the form to code that php will understand
         data: $('#frmRegister').serialize(),
         //return what? as json
@@ -13,10 +13,12 @@ $('#frmRegister').submit(function(){
         console.log(jData)
         if(jData.status == 1){
             console.log("you have signed up")
+            return
         }
 
         if(jData.status == 0){
             console.log("something went wrong")
+            return
         }
     }).
     fail(function(jData){
