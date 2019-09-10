a969a40acc3bb690dbb009ec14dbccac38600eae
Marija <mari4w04@stud.kea.dk>
2019-09-10 09:26:08 +0200

    added hasvoted switch

--- apis/api-vote.php
+++ apis/api-vote.php
@@ -21,7 +21,10 @@ $jVote = new stdClass(); //json object
 $jVote->voteId = uniqid();
 $jVote->candidateName = $sCandidate;
 
+
 $jInnerData->$sUser->vote = $jVote;
+$jInnerData->$sUser->hasVoted = true;
+
 
 file_put_contents('../voters.json', json_encode($jData, JSON_PRETTY_PRINT));
 
